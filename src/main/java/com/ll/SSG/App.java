e64d674281a53d625f34b4888932a9a95b99a967
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-11-06 00:12:54 +0900

    Feat : 삭제 기능

--- src/main/java/com/ll/SSG/App.java
+++ src/main/java/com/ll/SSG/App.java
@@ -13,17 +13,17 @@ public class App {
         List<WiseSay> wiseSays = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int id = 0;
+
         outer:
         while (true) {
             System.out.println("Command Example : '종료', '등록'");
             System.out.print("Please enter a command  : ");
 
-
             String cmd = br.readLine();
+            Rq rq = new Rq(cmd);
 
-            int id = 0;
-
-            switch(cmd) {
+            switch(rq.getCmd()) {
                 case "등록":
                     System.out.print("명언 : ");
                     String content = br.readLine().trim();
@@ -48,6 +48,38 @@ public class App {
                         System.out.printf("%d / %s / %s\n", getWiseSay.id, getWiseSay.content, getWiseSay.author);
                     }
                     break;
+
+                case "삭제":
+                    int deleteId = 0;
+                    if(cmd.equals("삭제")) {
+                        System.out.println("삭제할 ID를 입력해주세요.");
+                        deleteId = Integer.parseInt(br.readLine().trim());
+                    } else {
+                        deleteId = rq.getIntParam("id", 0);
+                    }
+
+                    if(deleteId == 0) {
+                        System.out.println("삭제할 ID를 입력해주세요.");
+                        continue;
+                    }
+
+                    WiseSay wiseSay_ = null;
+
+                    for (WiseSay getWiseSay : wiseSays) {
+                        if(getWiseSay.id == deleteId) {
+                            wiseSay_ = getWiseSay;
+                        }
+                    }
+
+                    if(wiseSay_ == null) {
+                        System.out.println("해당 ID의 명언이 존재하지 않습니다.");
+                        continue;
+                    }
+
+                    wiseSays.remove(wiseSay_);
+
+                    System.out.printf("%d번 명언이 삭제되었습니다.", deleteId);
+                    continue;
 
                 case "종료":
                     break outer;
---
+++ src/main/java/com/ll/SSG/Rq.java
@@ -0,0 +1,39 @@
+package com.ll.SSG;
+
+public class Rq {
+    String url;
+    String cmd;
+    String params;
+
+    public Rq(String url) {
+        this.url = url;
+
+
+        String[] cmdAndParam = url.split("\\?", 2);
+        this.cmd = cmdAndParam[0];
+
+        if(cmdAndParam.length == 2) {
+            this.params = cmdAndParam[1];
+        }
+    }
+
+    public int getIntParam(String paramName, int defaultValue) {
+        String[] param = params.split("&");
+
+        for(String paramBit : param) {
+            String[] keyAndValue = paramBit.split("=", 2);
+            String key = keyAndValue[0];
+            String value = keyAndValue[1];
+
+            if(paramName.equals(key)) {
+                return Integer.parseInt(value);
+            }
+        }
+
+        return defaultValue;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+}
